88290b819b26be1225f373933868ca4fc904ac78
Oscar Rodriguez <osrofer00@gmail.com>
2019-02-20 10:22:38 +0100

    2 devuelve 2

--- src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -12,6 +12,8 @@ package com.cifpcarballeira.katas.FizzBuzz;
 public class FizzBuzz {
     
     public String getFizzBuzz(int i){
-        return "1";
+        if (i == 2 ){
+            return "2";
+        }return "1";
     }
 }
