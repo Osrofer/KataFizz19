ce413019f180d0b369697c97b1de5542841b82ce
Oscar Rodriguez <osrofer00@gmail.com>
2019-02-20 10:13:20 +0100

    1 devuelve 1

--- test/com/carballeira/kata/FizzBuzz/tests/TestFizzBuzz.java
+++ test/com/carballeira/kata/FizzBuzz/tests/TestFizzBuzz.java
@@ -6,6 +6,7 @@ package com.carballeira.kata.FizzBuzz.tests;
  * and open the template in the editor.
  */
 
+import com.cifpcarballeira.katas.FizzBuzz.FizzBuzz;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -18,9 +19,12 @@ public class TestFizzBuzz {
     public TestFizzBuzz() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
+    
+    @Test
+    public void test1devuelve1() {
+        FizzBuzz fb = new FizzBuzz();
+        String resultado = fb.getFizzBuzz(1);
+        assertEquals("1", resultado);
+    }
 }
